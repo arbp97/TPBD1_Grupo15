b1f077b3cf7a9de875c68aa0fe58c519ea3e78e3
Alan <44730957+arbp97@users.noreply.github.com>
2020-09-08 12:40:58 -0300

    Update procedures.sql

--- procedures.sql
+++ procedures.sql
@@ -1,19 +1,19 @@
-CREATE PROCEDURE alta_concesionaria(nombre VARCHAR(100), direccion VARCHAR(100))
+CREATE PROCEDURE alta_concesionaria(@nombre VARCHAR(100), @direccion VARCHAR(100))
 BEGIN
 
 INSERT INTO 'concesionaria'(nombre,descripcion)
-VALUES(nombre,descripcion)
+VALUES(@nombre,@descripcion)
 
 END;
 
 
-CREATE PROCEDURE mod_concesionaria(id INT, nombre VARCHAR(100), direccion VARCHAR(100))
+CREATE PROCEDURE mod_concesionaria(@id INT, @nombre VARCHAR(100), @direccion VARCHAR(100))
 BEGIN
 
 UPDATE 'concesionaria'
   SET 
-    nombre = nombre,
-    direccion = direccion
-  WHERE id = id;
+    nombre = @nombre,
+    direccion = @direccion
+  WHERE id = @id;
 
 END;
