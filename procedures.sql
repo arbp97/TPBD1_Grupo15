6fa869bae56e34bed480da0bfa7b6a122b360838
arbp97 <arbp97@outlook.com>
2020-09-09 20:22:58 -0300

    procedimientos alta y mod concesionaria para ir probando

--- procedures.sql
+++ procedures.sql
@@ -1,19 +1,35 @@
-CREATE PROCEDURE alta_concesionaria(@nombre VARCHAR(100), @direccion VARCHAR(100))
+CREATE PROCEDURE alta_concesionaria(nombre VARCHAR(100), direccion VARCHAR(100))
 BEGIN
 
-INSERT INTO 'concesionaria'(nombre,descripcion)
-VALUES(@nombre,@descripcion)
+INSERT INTO concesionaria(nombre,direccion)
+VALUES(nombre,direccion);
 
 END;
 
 
-CREATE PROCEDURE mod_concesionaria(@id INT, @nombre VARCHAR(100), @direccion VARCHAR(100))
+CREATE PROCEDURE mod_concesionaria(id INT, nombre VARCHAR(100), direccion VARCHAR(100))
 BEGIN
+	
+	DECLARE new_nombre VARCHAR(100);
+	DECLARE new_direccion VARCHAR(100);
 
-UPDATE 'concesionaria'
+	IF nombre IS NULL OR nombre='' THEN	
+		-- SET new_nombre = SELECT concesionaria.nombre FROM concesionaria WHERE id = id;
+		SELECT concesionaria.nombre INTO new_nombre FROM concesionaria WHERE id = id;
+	ELSE 
+		SET new_nombre = nombre;
+	END IF;
+	IF direccion IS NULL OR direccion='' THEN	
+		-- SET new_direccion = SELECT concesionaria.direccion FROM concesionaria WHERE id = id;
+		SELECT concesionaria.direccion INTO new_direccion FROM concesionaria WHERE id = id;
+	ELSE 
+		SET new_direccion = direccion;
+	END IF;
+
+UPDATE concesionaria
   SET 
-    nombre = @nombre,
-    direccion = @direccion
-  WHERE id = @id;
+    nombre = new_nombre,
+    direccion = new_direccion
+  WHERE id = id;
 
 END;
