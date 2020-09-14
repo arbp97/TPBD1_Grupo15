f2224597ce97c671231c296f11aa680f38374b8e
Rapidgame7 <rapidgame7@gmail.com>
2020-09-14 18:15:35 -0300

    vehiculo, vehiculo x estacion y pedido insumo
    m

--- procedures.sql
+++ procedures.sql
@@ -174,4 +174,110 @@ DELETE FROM insumo WHERE id = id;
 END$$
 
 
+
+-- El Claudio
+
+-- ######## Vehi Culo ########
+
+DROP PROCEDURE IF EXISTS alta_vehiculo$$
+CREATE PROCEDURE alta_vehiculo(modid int, pedid int, finbit BIT)
+BEGIN
+	INSERT INTO vehiculo(modelo_id, pedido_venta_id, finalizado)
+	VALUES(modid,pedid,finbit);
+END$$
+DROP PROCEDURE IF EXISTS baja_vehiculo$$
+CREATE PROCEDURE baja_vehiculo(id int)
+BEGIN
+	DELETE FROM vehiculo WHERE num_chasis = id;
+END$$
+DROP PROCEDURE IF EXISTS mod_vehiculo$$
+CREATE PROCEDURE mod_vehiculo(id int, modid int, pedid int, finbit BIT)
+BEGIN
+	DECLARE new_modid,new_pedid int;
+    DECLARE new_finbit BIT;
+    
+    IF ISNULL(modid) THEN SELECT modelo_id INTO new_modid FROM vehiculo WHERE num_chasis = id;
+    ELSE SET new_modid = modid; END IF;
+    IF ISNULL(pedid) THEN SELECT pedido_venta_id INTO new_pedid FROM vehiculo WHERE num_chasis = id;
+    ELSE SET new_pedid = pedid; END IF;
+    IF ISNULL(finbit) THEN SELECT finalizado INTO new_finbit FROM vehiculo WHERE num_chasis = id;
+    ELSE SET new_finbit = finbit; END IF;
+    
+	UPDATE vehiculo SET
+		modelo_id = new_modid,
+		pedido_venta_id = new_pedid,
+		finalizado = new_finbit
+	WHERE num_chasis = id;
+END$$
+
+
+
+-- ######## Pedido Insumon ########
+
+DROP PROCEDURE IF EXISTS alta_pedido_insumo$$
+CREATE PROCEDURE alta_pedido_insumo(insid int, proid int, cant float)
+BEGIN
+	INSERT INTO pedido_insumo(insumo_id,proveedor_id,cantidad)
+	VALUES(insid,proid,cant);
+END$$
+DROP PROCEDURE IF EXISTS baja_pedido_insumo$$
+CREATE PROCEDURE baja_pedido_insumo(id int)
+BEGIN
+	DELETE FROM pedido_insumo WHERE id = id;
+END$$
+DROP PROCEDURE IF EXISTS mod_pedido_insumo$$
+CREATE PROCEDURE mod_pedido_insumo(id int, insid int, proid int, cant float)
+BEGIN
+	DECLARE new_insid,new_proid,new_cant int;
+    
+    IF ISNULL(insid) THEN SELECT insumo_id INTO new_insid FROM pedido_insumo WHERE id = id;
+    ELSE SET new_insid = insid; END IF;
+    IF ISNULL(proid) THEN SELECT proveedor_id INTO new_proid FROM pedido_insumo WHERE id = id;
+    ELSE SET new_proid = proid; END IF;
+    IF ISNULL(cant) THEN SELECT cantidad INTO new_cant FROM pedido_insumo WHERE id = id;
+    ELSE SET new_cant = cant; END IF;
+    
+	UPDATE pedido_insumo SET
+		insumo_id = new_insid,
+		proveedor_id = new_proid,
+		cantidad = new_cant
+	WHERE id = id;
+END$$
+
+
+
+-- ######## Vehi Culo Vs. Estacion - Round 1 ########
+
+DROP PROCEDURE IF EXISTS alta_vehiculo_x_estacion$$
+CREATE PROCEDURE alta_vehiculo_x_estacion(estid int, indate datetime, outdate datetime)
+BEGIN
+	INSERT INTO vehiculo_x_estacion(estacion_id, fecha_ingreso, fecha_egreso)
+	VALUES(estid,indate,outdate);
+END$$
+DROP PROCEDURE IF EXISTS baja_vehiculo_x_estacion$$
+CREATE PROCEDURE baja_vehiculo_x_estacion(id int)
+BEGIN
+	DELETE FROM vehiculo_x_estacion WHERE vehiculo_num_chasis = id;
+END$$
+DROP PROCEDURE IF EXISTS mod_vehiculo_x_estacion$$
+CREATE PROCEDURE mod_vehiculo_x_estacion(id int, estid int, indate datetime, outdate datetime)
+BEGIN
+	DECLARE new_estid int;
+    DECLARE new_indate,new_outdate datetime;
+    
+    IF ISNULL(estid) THEN SELECT estacion_id INTO new_estid FROM vehiculo_x_estacion WHERE vehiculo_num_chasis = id;
+    ELSE SET new_estid = estid; END IF;
+    IF ISNULL(indate) THEN SELECT fecha_ingreso INTO new_indate FROM vehiculo_x_estacion WHERE vehiculo_num_chasis = id;
+    ELSE SET new_indate = indate; END IF;
+    -- No hay chequeo para outdate - este puede ser nulo
+    
+	UPDATE vehiculo_x_estacion SET
+		estacion_id = new_estid,
+		fecha_ingreso = new_indate,
+		fecha_egreso = new_outdate
+	WHERE vehiculo_num_chasis = id;
+END$$
+
+
+
 DELIMITER ;
