e2b7037e2374c0628ce3c885d6d61916f3ad0529
Langleyy <43056222+Langleyy@users.noreply.github.com>
2020-10-07 22:39:38 -0300

    Algunos espacios

--- procedures.sql
+++ procedures.sql
@@ -374,48 +374,81 @@ END	$$
 DROP PROCEDURE IF EXISTS alta_vehiculo$$
 CREATE PROCEDURE alta_vehiculo(modid int, pedid int, finbit BIT)
 BEGIN
+
 	INSERT INTO vehiculo(modelo_id, pedido_venta_id, finalizado)
 	VALUES(modid,pedid,finbit);
+
 END$$
 
 DROP PROCEDURE IF EXISTS baja_vehiculo$$
 CREATE PROCEDURE baja_vehiculo(id int)
 BEGIN
+
 	DECLARE C INT DEFAULT 0;
 	DECLARE RES INT DEFAULT 0;
-    DECLARE MSG VARCHAR(69) DEFAULT "";
+        DECLARE MSG VARCHAR(69) DEFAULT "";
 
-    SELECT COUNT(num_chasis) INTO C FROM vehiculo WHERE vehiculo.num_chasis = id;
-    IF (C = 0) THEN
+        SELECT COUNT(num_chasis) INTO C FROM vehiculo WHERE vehiculo.num_chasis = id;
+
+        IF (C = 0) THEN
+
 		SET RES = 0;
-        SET MSG = "Pero eso no existe, pelotudo";
+                SET MSG = "Pero eso no existe, pelotudo";
 		-- SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'Pero eso no existe, pelotudo';
+
 	ELSE
+
 		DELETE FROM vehiculo WHERE num_chasis = id;
 		SET RES = 1;
 		SET MSG = "Vehiculo eliminado";
-    END IF;
-    SELECT RES AS nResultado, MSG as cMensaje;
+
+        END IF;
+
+        SELECT RES AS nResultado, MSG as cMensaje;
+
 END$$
 
 DROP PROCEDURE IF EXISTS mod_vehiculo$$
 CREATE PROCEDURE mod_vehiculo(id int, modid int, pedid int, finbit BIT)
 BEGIN
-	DECLARE new_modid,new_pedid int;
-    DECLARE new_finbit BIT;
+
+        DECLARE new_modid,new_pedid int;
+        DECLARE new_finbit BIT;
     
-    IF ISNULL(modid) THEN SELECT modelo_id INTO new_modid FROM vehiculo WHERE num_chasis = id;
-    ELSE SET new_modid = modid; END IF;
-    IF ISNULL(pedid) THEN SELECT pedido_venta_id INTO new_pedid FROM vehiculo WHERE num_chasis = id;
-    ELSE SET new_pedid = pedid; END IF;
-    IF ISNULL(finbit) THEN SELECT finalizado INTO new_finbit FROM vehiculo WHERE num_chasis = id;
-    ELSE SET new_finbit = finbit; END IF;
+        IF ISNULL(modid) THEN
+
+		SELECT modelo_id INTO new_modid FROM vehiculo WHERE num_chasis = id;
+
+        ELSE 
+
+                SET new_modid = modid; 
+
+        END IF;
+
+        IF ISNULL(pedid) THEN
+
+                 SELECT pedido_venta_id INTO new_pedid FROM vehiculo WHERE num_chasis = id;
+
+        ELSE 
+                 SET new_pedid = pedid; 
+
+        END IF;
+
+        IF ISNULL(finbit) THEN
+
+                 SELECT finalizado INTO new_finbit FROM vehiculo WHERE num_chasis = id;
+
+        ELSE 
+                 SET new_finbit = finbit; 
+
+        END IF;
     
 	UPDATE vehiculo SET
-		modelo_id = new_modid,
-		pedido_venta_id = new_pedid,
-		finalizado = new_finbit
+                 modelo_id = new_modid,
+                 pedido_venta_id = new_pedid,
+                 finalizado = new_finbit
 	WHERE num_chasis = id;
+
 END$$
 
 -- PEDIDO_INSUMO
@@ -423,43 +456,81 @@ END$$
 DROP PROCEDURE IF EXISTS alta_pedido_insumo$$
 CREATE PROCEDURE alta_pedido_insumo(insid int, proid int, cant float)
 BEGIN
+
 	INSERT INTO pedido_insumo(insumo_id,proveedor_id,cantidad)
 	VALUES(insid,proid,cant);
+
 END$$
 
 DROP PROCEDURE IF EXISTS baja_pedido_insumo$$
 CREATE PROCEDURE baja_pedido_insumo(id int)
 BEGIN
+
 	DECLARE C INT DEFAULT 0;
 	DECLARE RES INT DEFAULT 0;
-    DECLARE MSG VARCHAR(69) DEFAULT "";
-    SELECT COUNT(id) INTO C FROM pedido_insumo WHERE id = id;
-    IF (C = 0) THEN
-		SET RES = -420;
-        SET MSG = "No se encontraron pedido_insumos con esa ID";
+        DECLARE MSG VARCHAR(69) DEFAULT "";
+
+        SELECT COUNT(id) INTO C FROM pedido_insumo WHERE id = id;
+
+        IF (C = 0) THEN
+
+                 SET RES = -420;
+                 SET MSG = "No se encontraron pedido_insumos con esa ID";
+
 	ELSE
-		DELETE FROM pedido_insumo WHERE id = id;
-    END IF;
-    SELECT RES AS nResultado, MSG as cMensaje;
+
+                 DELETE FROM pedido_insumo WHERE id = id;
+
+        END IF;
+
+        SELECT RES AS nResultado, MSG as cMensaje;
+
 END$$
 
 DROP PROCEDURE IF EXISTS mod_pedido_insumo$$
 CREATE PROCEDURE mod_pedido_insumo(id int, insid int, proid int, cant float)
 BEGIN
-	DECLARE new_insid,new_proid,new_cant int;
+
+        DECLARE new_insid,new_proid,new_cant int;
     
-    IF ISNULL(insid) THEN SELECT insumo_id INTO new_insid FROM pedido_insumo WHERE id = id;
-    ELSE SET new_insid = insid; END IF;
-    IF ISNULL(proid) THEN SELECT proveedor_id INTO new_proid FROM pedido_insumo WHERE id = id;
-    ELSE SET new_proid = proid; END IF;
-    IF ISNULL(cant) THEN SELECT cantidad INTO new_cant FROM pedido_insumo WHERE id = id;
-    ELSE SET new_cant = cant; END IF;
+        IF ISNULL(insid) THEN 
+
+                 SELECT insumo_id INTO new_insid FROM pedido_insumo WHERE id = id;
+
+        ELSE 
+
+                 SET new_insid = insid;
+ 
+        END IF;
+
+        IF ISNULL(proid) THEN
+
+                 SELECT proveedor_id INTO new_proid FROM pedido_insumo WHERE id = id;
+
+        ELSE 
+
+                 SET new_proid = proid; 
+
+        END IF;
+
+        IF ISNULL(cant) THEN 
+
+                 SELECT cantidad INTO new_cant FROM pedido_insumo WHERE id = id;
+
+        ELSE 
+
+                 SET new_cant = cant; 
+
+        END IF;
     
 	UPDATE pedido_insumo SET
+
 		insumo_id = new_insid,
 		proveedor_id = new_proid,
 		cantidad = new_cant
+
 	WHERE id = id;
+
 END$$
 
 -- VEHICULO_X_ESTACION
@@ -467,25 +538,36 @@ END$$
 DROP PROCEDURE IF EXISTS alta_vehiculo_x_estacion$$
 CREATE PROCEDURE alta_vehiculo_x_estacion(chaid int, estid int, indate datetime, outdate datetime)
 BEGIN
+
 	INSERT INTO vehiculo_x_estacion(vehiculo_num_chasis, estacion_id, fecha_ingreso, fecha_egreso)
 	VALUES(chaid,estid,indate,outdate);
+
 END$$
 
 DROP PROCEDURE IF EXISTS baja_vehiculo_x_estacion$$
 CREATE PROCEDURE baja_vehiculo_x_estacion(chasisid int, stationid int)
 BEGIN
+
 	DECLARE C INT DEFAULT 0;
 	DECLARE RES INT DEFAULT 0;
-    DECLARE MSG VARCHAR(69) DEFAULT "";
-    SELECT COUNT(id) INTO C FROM vehiculo_x_estacion WHERE vehiculo_num_chasis = chasisid AND estacion_id = stationid;
-    IF (C = 0) THEN
-		SET RES = -666;
-        SET MSG = "No se encuentran resultados para el par de IDs";
+	DECLARE MSG VARCHAR(69) DEFAULT "";
+
+	SELECT COUNT(id) INTO C FROM vehiculo_x_estacion WHERE vehiculo_num_chasis = chasisid AND estacion_id = stationid;
+
+	IF (C = 0) THEN
+
+                 SET RES = -666;
+                 SET MSG = "No se encuentran resultados para el par de IDs";
+
 	ELSE
-		-- DELETE FROM pedido_insumo WHERE id = id;
-        DELETE FROM vehiculo_x_estacion WHERE vehiculo_num_chasis = chasisid AND estacion_id = stationid;
-    END IF;
-    SELECT RES AS nResultado, MSG as cMensaje;
+
+                 -- DELETE FROM pedido_insumo WHERE id = id;
+                 DELETE FROM vehiculo_x_estacion WHERE vehiculo_num_chasis = chasisid AND estacion_id = stationid;
+
+	END IF;
+
+	SELECT RES AS nResultado, MSG as cMensaje;
+
 END$$
 
 DROP PROCEDURE IF EXISTS mod_vehiculo_x_estacion$$
