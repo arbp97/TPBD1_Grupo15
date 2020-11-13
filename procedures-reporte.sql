94108d2ae8f1c2dc693101d62c80b2af276cf28b
Rapidgame7 <rapidgame7@gmail.com>
2020-11-12 22:46:02 -0300

    Base para los otros dos puntos

--- procedures-reporte.sql
+++ procedures-reporte.sql
@@ -1,14 +1,12 @@
 DELIMITER $$
-set @testPedido = 1$$
+set @testNumero = 1$$
 
 /*
 	Dado un numero de pedido, se requiere listar los vehiculos indicando el chasis,
     si se encuentra finalizado, y si no esta terminado, indicar en que estacion se encuentra.
-    
-    
 */
 DROP PROCEDURE IF EXISTS reporte_vehiculos$$
-CREATE PROCEDURE reporte_vehiculos(iPedido int) -- NO FUNCIONA, ARREGLAR!!
+CREATE PROCEDURE reporte_vehiculos(iPedido int)
 proc: BEGIN
 	DECLARE C int default 0;
     
@@ -29,7 +27,36 @@ proc: BEGIN
     ) vxe on v.num_chasis = vxe.vnchasis;
    
 END $$
-call reporte_vehiculos(1)$$ -- Prueba rapida (avanzar algunos chasis para mostrar!)
+call reporte_vehiculos(1)$$ -- Prueba rapida
+
+/*
+	Dado un numero de pedido, se requiere listar los insumos que sera necesario
+    solicitar, indicando codigo de insumo y cantidad requerida para ese pedido.
+*/
+DROP PROCEDURE IF EXISTS reporte_insumos$$
+CREATE PROCEDURE reporte_insumos(iPedido int)
+proc: BEGIN
+	DECLARE C int default 0;
+    
+	
+   
+END $$
+call reporte_insumos(1)$$ -- Prueba rapida
+
+/*
+	Dada una linea de montaje, indicar el tiempo promedio de construccion de los vehiculos
+    (tener en cuenta solo los vehiculos terminados)
+    Che Alan, podes poner el nombre del procedimiento en español y que sea legible? Gracias <3
+*/
+DROP PROCEDURE IF EXISTS reporte_buildtime$$
+CREATE PROCEDURE reporte_buildtime(iLinea int)
+proc: BEGIN
+	DECLARE C int default 0;
+    
+	
+   
+END $$
+call reporte_buildtime(1)$$ -- Prueba rapida
 
 DELIMITER ;
 
